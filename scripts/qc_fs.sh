cbb2ecec6cfeab770a11cf71b1c21ef4ebf96cde
ShrutiGadewar <39843804+ShrutiGadewar@users.noreply.github.com>
2021-03-31 18:46:46 -0700

    Update qc_fs.sh

--- scripts/qc_fs.sh
+++ scripts/qc_fs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #$ -S /bin/bash
 
-
-
 # Python with required libraries
 py_path=./Anaconda3/bin/python
 
@@ -11,9 +9,6 @@ dirFS=./cross_section/sc_cross_section
 
 #Path to the subject list
 dirSub=./data/subj.txt
-
-#Directory where the aparc measures are saved
-dirI=./data
 
 #Output directory where the QC predictions are saved
 dirO=./results
@@ -26,12 +21,12 @@ cd ${script}
 
 
 
+
+#For FS version 5.3, use,
+sh aparc_metrics.sh -o ${dirO}/aparc.csv -f ${dirFS} -s $(cat ${dirSub})
+
 #For FS version 7.1.1, use,
 # sh aparc_metrics_FS7.sh -o ${dirI}/aparc.csv -f ${dirFS} -s $(cat ${dirSub})
 
-#For FS version 5.3, use,
-sh aparc_metrics.sh -o ${dirI}/aparc.csv -f ${dirFS} -s $(cat ${dirSub})
-
 #For generating QC predictions
-${py_path} qc.py --inp ${dirI}/aparc.csv --out ${dirO}/QC_predictions.csv
-
+${py_path} qc.py --inp ${dirO}/aparc.csv --out ${dirO}/QC_predictions.csv
